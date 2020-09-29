93faf9a8c20b2827994d72aa51c9439dc0030bfd
B. Drawe <brycerdrawe.b@gmail.com>
2020-09-29 00:55:58 -0600

    feature --- getInventory and getStats functionality

--- __test__/Player.test.js
+++ __test__/Player.test.js
@@ -4,8 +4,6 @@ const Potion = require('../lib/__mocks__/Potion');
 
 jest.mock('../lib/Potion');
 
-console.log(new Potion());
-
 test('creates a player object', () =>{
     const player = new Player('Dave');
 
@@ -19,4 +17,23 @@ test('creates a player object', () =>{
     expect(player.agility).toEqual(expect.any(Number));
     
 });
+
+test('creates a player stats as an object', () =>{
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+  
+    expect(player.getInventory()).toEqual(expect.any(Array));
+  
+    player.inventory = [];
+  
+    expect(player.getInventory()).toEqual(false);
+  });
 
